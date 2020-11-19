34bacc800e2cb5cd481389c4de398c09eeea47d9
Kevin St.Onge <kevinstonge@gmail.com>
2020-11-19 17:40:29 -0500

    mvp and three stretches complete

--- recipes/recipeRouter.js
+++ recipes/recipeRouter.js
@@ -17,4 +17,22 @@ router.get('/:id', async (req, res) => {
         res.status(500).json({ error })
     }
 })
+
+router.get('/:id/shoppingList', async (req, res) => {
+    try {
+        const shoppingList = await recipeModel.getShopppingList(req.params.id);
+        res.status(200).json({shoppingList})
+    } catch (error) {
+        res.status(500).json({ error })
+    }
+})
+
+router.get('/:id/instructions', async (req, res) => {
+    try {
+        const instructions = await recipeModel.getInstructions(req.params.id);
+        res.status(200).json({ instructions });
+    } catch (error) {
+        res.status(500).json({ error })
+    }
+})
 module.exports = router;
